2c5bd21d4750edf4cb1bf70e948c1be9b62baa25
Sune Radich Christensen <sykokaj@gmail.com>
2012-06-10 13:50:05 +0200

    Added about page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
-  end
+	describe "Home Page" do
+		it "should have the content 'SampleApp'" do
+			visit '/static_pages/home'
+			page.should have_content('Sample App')
+		end
+	end
+
+	describe "About Page" do
+		it "should have the content 'About'" do
+			visit '/static_pages/about'
+			page.should have_content('About')
+		end
+	end
+
+	describe "Help Page" do
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			page.should have_content('Help')
+		end
+	end
 end
