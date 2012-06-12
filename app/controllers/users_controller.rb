4c21f607cb401ae79a8ba26a5f51d72000e18d5b
Sune Radich Christensen <sykokaj@gmail.com>
2012-06-12 19:52:51 +0200

    Finished user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,19 @@ class UsersController < ApplicationController
  		@user = User.find(params[:id])
 	end
 
-  def new
-  	@user = User.new
-  end
+	def new
+  		@user = User.new
+  	end
+
+	def create
+		@user = User.new(params[:user])
+		
+		if @user.save
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
 
 end
